afc8514c97abc0361c52ee694a83de2935e780ae
richardpathadan-coder <richardpathadan@gmail.com>
2026-01-24 21:17:58 +0100

    latest

--- project_sql/3_Top_demanded_skills(Normal_Query).sql
+++ project_sql/3_Top_demanded_skills(Normal_Query).sql
@@ -4,13 +4,13 @@ Top 5 demanded skills for remote jobs by (job_location = 'Anywhere')
 */
 
 SELECT 
-skills_dim.skills,
-count (job_postings_fact.job_id) as job_count
+skills,
+count (skills_job_dim.job_id) as job_count
 from job_postings_fact
-LEFT JOIN skills_job_dim on job_postings_fact.job_id=skills_job_dim.job_id
-LEFT JOIN skills_dim on skills_job_dim.skill_id=skills_dim.skill_id
-WHERE job_postings_fact.job_location= 'Anywhere'
-GROUP BY skills_dim.skills
+left JOIN skills_job_dim on job_postings_fact.job_id=skills_job_dim.job_id
+LEFT join skills_dim on skills_job_dim.skill_id=skills_dim.skill_id
+WHERE job_work_from_home = True AND job_postings_fact.job_title_short= 'Data Analyst'
+GROUP BY skills
 ORDER BY job_count DESC
 limit 5;
 
--- project_sql/4_Top_demanded_skills( withCTE).sql
+++ project_sql/4_Top_demanded_skills( withCTE).sql
@@ -1,3 +1,20 @@
-SELECT *
+/*
+Top 5 demanded skills for remote jobs
+*/
+
+with remote_job_skills as (SELECT 
+skills_job_dim.skill_id,
+count(*) as job_count
 FROM skills_job_dim
-limit 10;
+inner join job_postings_fact on skills_job_dim.job_id=job_postings_fact.job_id
+WHERE job_work_from_home = 'True' and job_title_short = 'Data Analyst'
+GROUP BY skill_id)
+
+SELECT 
+skills_dim.skill_id,
+skills_dim.skills,
+job_count
+FROM remote_job_skills
+inner join skills_dim on remote_job_skills.skill_id=skills_dim.skill_id
+ORDER by job_count DESC
+LIMIT 5;
